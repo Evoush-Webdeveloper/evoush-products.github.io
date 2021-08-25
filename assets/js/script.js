5f9bd4cc54789bf2d553904c99d6f5ed7c4e1362
codesyariah122 <pujiermanto@gmail.com>
2021-08-25 21:54:39 +0700

    update responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
 function imgSlider(anything, color){
 	document.querySelector('.starbucks').src = anything;
-	document.querySelector('.circle').style.backgroundColor = color;
+}
+
+function changeCircleColor(color){
+	const circle = document.querySelector('.circle')
+	circle.style.backgroundColor = color
+}
+
+
+function toggleMenu(){
+	const menuToggle = document.querySelector('.toggle')
+	const navigation = document.querySelector('.navigation')
+	menuToggle.classList.toggle('active')
+	navigation.classList.toggle('active')
 }
