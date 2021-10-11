f3870d8e3aeb6e14b5c2863118b9a340beaf3c4b
codesyariah122 <pujiermanto@gmail.com>
2021-10-11 12:55:50 +0700

    update with loading

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,32 +1,33 @@
-const allproduct = async(url) => {
-    const products = await fetch(url)
-    return products.json()
+const allproduct = async (url) => {
+  const products = await fetch(url);
+  return products.json();
+};
+
+function FirstProduct(first) {
+  CreateElement("div", first, "first", "#e397a3");
 }
 
-function FirstProduct(first){
-    CreateElement('div', first, 'first', '#e397a3')
+function SecondProduct(second) {
+  CreateElement("div", second, "second", "#1c6242");
 }
 
-function SecondProduct(second){
-    CreateElement('div', second, 'second', '#1c6242')
+function ThirdProduct(third) {
+  CreateElement("div", third, "third", "salmon");
 }
 
-function ThirdProduct(third){
-    CreateElement('div', third, 'third', 'salmon')
-}
-
-
-function CreateElement(el, data, dom, color){
-    // console.log(data)
-    const newEl = document.createElement('div')
-    newEl.className='container'
-    newEl.innerHTML = `
+function CreateElement(el, data, dom, color) {
+  // console.log(data);
+  const newEl = document.createElement("div");
+  newEl.className = "container";
+  newEl.innerHTML = `
         <img src="${data.image.url}"/>
         <br>
         <div class="middle" style="background-color:${color};">
-			<div class="text">${data.seo.title}</div>
+			<div class="text">
+                <h4>${data.seo.title}</h4>
+                <p>${data.seo.description}</p>
+            </div>
 		</div>
-    `
-    document.getElementById(dom).appendChild(newEl)
+    `;
+  document.getElementById(dom).appendChild(newEl);
 }
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,34 @@
 const url = "https://evoush-mock-api.herokuapp.com/products";
+const loading = document.getElementById("loading");
+loading.style.visibility = "hidden";
 
 allproduct(url)
   .then((res) => {
+    console.log(res.data.length);
     const products = res.data.slice().reverse();
     const firsts = products.slice(0, 5);
     const seconds = products.slice(4, 9);
     const thirds = products.slice(9, 13);
 
-    firsts.map((first) => {
-      FirstProduct(first);
-    });
+    if (res.data.length > 0) {
+      loading.style.visibility = "visible";
+      setTimeout(function () {
+        loading.style.visibility = "hidden";
+        firsts.map((first) => {
+          FirstProduct(first);
+        });
 
-    seconds.map((second) => {
-      SecondProduct(second);
-    });
+        seconds.map((second) => {
+          SecondProduct(second);
+        });
 
-    thirds.map((third) => {
-      ThirdProduct(third);
-    });
+        thirds.map((third) => {
+          ThirdProduct(third);
+        });
+      }, 1500);
+    } else {
+      loading.style.visibility = "visible";
+    }
   })
   .catch((err) => {
     console.log(err);
